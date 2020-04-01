3d257259934882123b28e318e2efdbfa5d6f3245
S Praveen Kumar <praveens.bec@gmail.com>
2020-04-01 19:32:30 +0530

    Add Hello World

---
+++ 1 Hello World/hello.c
@@ -0,0 +1,15 @@
+/************************************************
+*   Copyright 2020 Praveen Kumar S
+*   Licensed under MIT License
+*
+*   Program to print Hello World
+*************************************************/
+
+//Include Standard Input Output library
+#include<stdio.h>
+
+//Entry point of our program
+int main(){
+    printf("Hello World\n");    //Prints Hello World and moves to next line
+    return 0;                   //Returning 0 indicates successful execution
+}
